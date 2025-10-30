2ed8735cab0bd2b27fadd4a34f62c80eccb8dd97
AlessandroMessineo <alessandromessineo46@gmail.com>
2025-10-30 15:37:37 +0100

    creato routers

---
+++ routers/movieRouter.js
@@ -0,0 +1,14 @@
+// importo express
+const express = require("express");
+
+// settiamo il router
+const router = express.Router();
+
+// index
+router.get('/', );
+
+// show
+router.get('/:id', );
+
+// esporto router
+module.exports = router;
